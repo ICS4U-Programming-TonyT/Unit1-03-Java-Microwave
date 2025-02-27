1324af122c524950a215a0921f5bb505e6a9e7c6
Tony Tran <tony.tran1@stu.ocsb.ca>
2025-02-27 15:38:19 +0000

    Modified Microwave.java

--- Microwave.java
+++ Microwave.java
@@ -24,16 +24,18 @@ final class Microwave {
     }
 
     /** Constant heating time for a sub. */
-    private static final int SUB_TIME = 60;
+    private static final int SUBTIME = 60;
     /** Constant heating time for a pizza. */
-    private static final int PIZZA_TIME = 45;
+    private static final int PIZZATIME = 45;
     /** Constant heating time for a soup. */
-    private static final int SOUP_TIME = 105;
+    private static final int SOUPTIME = 105;
 
     /** Multiplier for heating 2 items. */
-    private static final double MULTIPLIER_TWO = 1.5;
+    private static final double MULTIPLIERTWO = 1.5;
     /** Multiplier for heating 3 items. */
-    private static final double MULTIPLIER_THREE = 2.0;
+    private static final double MULTIPLIERTHREE = 2.0;
+    /** Maximum quantity amount.*/
+    private static final int MAXIMUMAMOUNT = 3;
 
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -42,13 +44,13 @@ final class Microwave {
         int baseTime = 0;
         switch (food) {
             case "sub":
-                baseTime = SUB_TIME;
+                baseTime = SUBTIME;
                 break;
             case "pizza":
-                baseTime = PIZZA_TIME;
+                baseTime = PIZZATIME;
                 break;
             case "soup":
-                baseTime = SOUP_TIME;
+                baseTime = SOUPTIME;
                 break;
             default:
                 System.out.println("The food must be sub, pizza, or soup.");
@@ -59,12 +61,12 @@ final class Microwave {
         System.out.print("How many are you cooking? (max: 3): ");
         try {
             int amount = Integer.parseInt(scanner.nextLine());
-            if (amount < 1f || amount > 3f) {
+            if (amount < 1 || amount > MAXIMUMAMOUNT) {
                 System.out.println("The quantity must be 1 and 3.");
             } else {
                 // Adjust heating time on quantity
-                double multiplier = (amount == 2) ? MULTIPLIER_TWO
-                : (amount == 3) ? MULTIPLIER_THREE : 1;
+                double multiplier = (amount == 2) ? MULTIPLIERTWO
+                : (amount == 3) ? MULTIPLIERTHREE : 1;
                 int totalTime = (int) (baseTime * multiplier);
 
                 // Converting time to minutes and seconds
