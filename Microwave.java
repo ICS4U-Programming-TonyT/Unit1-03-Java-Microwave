1d2ced3b7f86fbad8eecb38ad9d272673018c50a
Tony Tran <tony.tran1@stu.ocsb.ca>
2025-02-27 16:03:37 +0000

    Modified Microwave.java

--- Microwave.java
+++ Microwave.java
@@ -24,22 +24,22 @@ final class Microwave {
     }
 
     /** Constant heating time for a sub. */
-    private static final int SUBTIME = 60;
+    private static final int SUB_TIME = 60;
     /** Constant heating time for a pizza. */
-    private static final int PIZZATIME = 45;
+    private static final int PIZZA_TIME = 45;
     /** Constant heating time for a soup. */
-    private static final int SOUPTIME = 105;
+    private static final int SOUP_TIME = 105;
 
     /** Multiplier for heating 2 items. */
-    private static final double MULTIPLIERTWO = 1.5;
+    private static final double MULTIPLIER_TWO = 1.5;
     /** Multiplier for heating 3 items. */
-    private static final double MULTIPLIERTHREE = 2.0;
+    private static final double MULTIPLIER_THREE = 2.0;
     /** Maximum quantity amount.*/
-    private static final int MAXIMUMAMOUNT = 3;
+    private static final int MAXIMUM_AMOUNT = 3;
     /** Minimum quantity amount.*/
-    private static final int MINIMUMAMOUNT = 1;
+    private static final int MINIMUM_AMOUNT = 1;
     /** Amount to divide for Minutes and Seconds.*/
-    private static final int TIMEDIVIDE = 60;
+    private static final int TIME_DIVIDE = 60;
 
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -48,13 +48,13 @@ final class Microwave {
         int baseTime = 0;
         switch (food) {
             case "sub":
-                baseTime = SUBTIME;
+                baseTime = SUB_TIME;
                 break;
             case "pizza":
-                baseTime = PIZZATIME;
+                baseTime = PIZZA_TIME;
                 break;
             case "soup":
-                baseTime = SOUPTIME;
+                baseTime = SOUP_TIME;
                 break;
             default:
                 System.out.println("The food must be sub, pizza, or soup.");
@@ -65,17 +65,17 @@ final class Microwave {
         System.out.print("How many are you cooking? (max: 3): ");
         try {
             int amount = Integer.parseInt(scanner.nextLine());
-            if (amount < MINIMUMAMOUNT || amount >  MAXIMUMAMOUNT) {
+            if (amount < MINIMUM_AMOUNT || amount >  MAXIMUM_AMOUNT) {
                 System.out.println("The quantity must be 1 and 3.");
             } else {
                 // Adjust heating time on quantity
-                double multiplier = (amount == 2) ? MULTIPLIERTWO
-                : (amount == MAXIMUMAMOUNT) ? MULTIPLIERTHREE : 1;
+                double multiplier = (amount == 2) ? MULTIPLIER_TWO
+                : (amount == TIME_DIVIDE) ? MULTIPLIER_THREE : 1;
                 int totalTime = (int) (baseTime * multiplier);
 
                 // Converting time to minutes and seconds
-                int minutes = totalTime / TIMEDIVIDE;
-                int seconds = totalTime % TIMEDIVIDE;
+                int minutes = totalTime / TIME_DIVIDE;
+                int seconds = totalTime % TIME_DIVIDE;
 
                 // Displaying result
                 System.out.println("The total heating time is: "
